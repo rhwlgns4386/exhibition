1db31e9ab998aa81627eac692aa5396bd2cc4252
hunko <rhwlgns4386@naver.com>
2021-06-05 16:14:41 +0900

    init commit

---
+++ src/main/java/com/example/exhibition/ExhibitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExhibitionApplication.class, args);
+    }
+
+}
